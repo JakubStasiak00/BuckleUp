bd65e1a6145e9e34b3054ed92a6861faf4f37161
Jakub Stasiak <jakub.stasiak.front@gmail.com>
2024-10-16 18:55:54 +0200

    initial setup and landing page prototype

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,124 @@
+<template>
+  <q-layout class="glb">
+    <header>
+      <h1 class="logo"> Buckle<span class="logo-second">UP</span></h1>
+      <nav v-if="!$q.screen.lt.md">
+        <ul class="menu">
+          <li>
+            <q-icon class="icon" name="person" />
+            <a href="#" rel="noopener noreferrer">Join</a>
+          </li>
+          <li>
+            <q-icon class="icon" name="credit_card" />
+            <a href="#" rel="noopener noreferrer">Membership</a>
+          </li>
+          <li>
+            <q-icon class="icon" name="fitness_center" />
+            <a href="#" rel="noopener noreferrer">Showcase</a>
+          </li>
+          <li>
+            <q-icon class="icon" name="pin_drop" />
+            <a href="#" rel="noopener noreferrer">Location</a>
+          </li>
+        </ul>
+      </nav>
+      <nav v-else>
+        <q-icon class="hamburger" name="menu" />
+      </nav>
+    </header>
+
+    <q-page-container class="content">
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { useQuasar } from 'quasar';
+
+
+const $q = useQuasar()
+
+</script>
+
+<style scoped lang="scss">
+@font-face {
+  font-family: Obrazec;
+  src: url('../assets/fonts/Obrazec\ 2.0.otf');
+}
+
+.icon {
+  color: white;
+}
+
+.menu {
+  display: flex;
+  padding-right: 1rem;
+
+  li {
+    padding-inline: 1rem;
+    font-size: 1.2rem;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    z-index: 10;
+    transition: all 0.25s ease-in-out;
+  }
+
+  a {
+    color: $text-primary;
+    transition: all 0.25s ease-in-out;
+  }
+
+  li:hover {
+    transform: translateY(-5px);
+    z-index: 9;
+
+    a {
+      color: $text-accent;
+    }
+  }
+
+  li>.icon {
+    color: $text-accent;
+  }
+}
+
+.hamburger {
+  font-size: 1.5rem;
+  color: $text-primary;
+}
+
+header {
+  width: 100%;
+  position: relative;
+  padding: 0.3rem 1rem 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.logo {
+  font-family: Obrazec;
+  color: $text-primary;
+  font-size: clamp(2rem, 1.2rem + 4vw, 5rem);
+}
+
+.logo-second {
+  color: $text-accent;
+}
+
+.glb {
+  min-height: 100vh;
+  background-image: linear-gradient(to left, rgba(0, 0, 0, 0.85), rgba(0, 0, 0, 0.85), rgba(0, 0, 0, 0)), url('../assets/photos/landing_desktop.jpg');
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+}
+
+.content {
+  min-height: 100%;
+  flex: 1;
+  display: flex;
+}
+</style>
---
+++ src/pages/LandingPage.vue
@@ -0,0 +1,81 @@
+<template>
+    <main>
+        <div class="landing">
+            <h2>Transform Your Body, Transform Your Life.</h2>
+            <p>Join us in a journey of transformation where every workout is an opportunity to grow stronger. Together,
+                we’ll build a foundation of health, resilience, and confidence that lasts a lifetime.</p>
+            <div class="bttns">
+                <router-link to="/" class="btn btn-first">Join us</router-link>
+                <router-link to="/" class="btn btn-second">See Equipment</router-link>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+main {
+    flex: 1;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+}
+
+.landing {
+    text-align: center;
+    max-width: min(80%, 500px);
+    margin-inline: auto;
+    text-wrap: balance;
+
+    h2 {
+        color: $text-accent;
+        font-weight: 300;
+        font-size: clamp(1.5rem, 0.6rem + 2.5vw, 3rem);
+        line-height: clamp(1.5rem, 0.6rem + 2.5vw, 3rem);
+    }
+
+    p {
+        color: $text-primary;
+        font-weight: 300;
+        font-size: clamp(0.8rem, 0.62rem + 1.2vw, 1.7rem);
+    }
+
+    .bttns {
+        display: flex;
+        gap: 1rem;
+        justify-content: center;
+    }
+
+    .btn {
+        text-transform: uppercase;
+        text-decoration: none;
+        font-size: clamp(0.6rem, 0.48rem + 0.5vw, 1.2rem);
+        border-radius: 5px;
+        padding: clamp(0.6rem, 0.48rem + 0.5vw, 1.2rem);
+        color: $text-primary;
+        border: 1px solid $text-primary;
+        transition: all 0.25s ease-in-out;
+        transition-property: color, border-color, transform;
+
+        &:hover {
+            color: $text-accent;
+            border-color: $text-accent;
+            transform: translateY(-5px);
+        }
+    }
+
+
+}
+
+
+@media screen and (min-width: 1024px) {
+    .landing {
+        max-width: 40vw;
+        margin-right: 10rem;
+        text-align: left;
+    }
+}
+</style>
